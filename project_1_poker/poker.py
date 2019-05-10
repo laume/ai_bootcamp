abd1c67842ce813e4cb08c6cb80980db82287fa9
Martynas Jurkus <martynas@vinted.com>
2019-05-10 15:36:35 +0300

    Finish up hand detection

--- project_1_poker/poker.py
+++ project_1_poker/poker.py
@@ -3,144 +3,161 @@ import itertools
 
 suits = ['♠', '♦', '♥', '♣']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 ante = 10
 
-class Card:
-	
-	def __init__(self,suit,rank):
-		self.suit = suit
-		self.rank = rank
 
-	def __repr__(self):
-		return self.rank + self.suit[0]
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __repr__(self):
+        return self.rank + self.suit[0]
+
+    def __lt__(self, other):
+        return self.value < other.value
+
 
 class Deck:
-	
-	def __init__(self):
-		self.deck = [Card(suit, rank) for suit in suits for rank in ranks]
+    def __init__(self):
+        self.deck = [Card(suit, rank) for suit in suits for rank in ranks]
 
-	def shuffle(self):
-		random.shuffle(self.deck)
+    def shuffle(self):
+        random.shuffle(self.deck)
 
-	def deal_card(self):
-		return self.deck.pop()
+    def deal_card(self):
+        return self.deck.pop()
+
 
 class Hand:
+    def __init__(self, cards):
+        self.cards = sorted(list(cards))
 
-	def __init__(self,cards):
-		self.cards = cards
+    def combination(self):
+        if self.is_royal_flush():
+            return "Royal Flush! {}".format(self.cards)
+        if self.is_straight_flush():
+            return "Straight flush! {}".format(self.cards)
+        if self.is_full_house():
+            return "Full house! {}".format(self.cards)
+        if self.is_flush():
+            return "Flush! {}".format(self.cards)
+        if self.is_straight():
+            return "Straight {}".format(self.cards)
+        if self.is_3_of_a_kind():
+            return "Three of a kind! {}".format(self.cards)
+        if self.is_pair():
+            return "Pair! {}".format(self.cards)
+        else:
+            return 'High card {}'.format(self.cards)
 
-	def combination(self):
-		if self.is_flush():
-			return "Flush! {}".format(self.cards)
-		if self.is_3_of_a_kind():
-			return "Three of a kind! {}".format(self.cards)
-		if self.is_pair():
-			return "Pair! {}".format(self.cards)
-		else:
-			return 'dud'
+    def __repr__(self):
+        return " ".join(str(card) for card in self.cards)
 
-	def __repr__(self):
-		return " ".join(str(card) for card in self.cards)
+    def is_pair(self):
+        ranks = list(map(lambda card: card.rank, self.cards))
+        counts = dict((rank, ranks.count(rank)) for rank in ranks)
+        return any(count == 2 for value, count in counts.items())
 
-	def is_pair(self):
-		ranks = list(map(lambda card: card.rank, self.cards))
-		counts = dict((rank, ranks.count(rank)) for rank in ranks)
-		return any(count == 2 for value, count in counts.items())
+    def is_3_of_a_kind(self):
+        ranks = list(map(lambda card: card.rank, self.cards))
+        counts = dict((rank, ranks.count(rank)) for rank in ranks)
+        return any(count == 3 for value, count in counts.items())
 
-	def is_3_of_a_kind(self):
-		ranks = list(map(lambda card: card.rank, self.cards))
-		counts = dict((rank, ranks.count(rank)) for rank in ranks)
-		return any(count == 3 for value, count in counts.items())
+    def is_straight(self):
+        return all(x.value == y.value - 1 for x, y in zip(self.cards, self.cards[1:]))
 
-	def is_straight(self):
-		pass
+    def is_flush(self):
+        return len(set(map(lambda card: card.suit, self.cards))) == 1
 
-	def is_flush(self):
-		return len(set(map(lambda card: card.suit, self.cards))) == 1
+    def is_full_house(self):
+        return self.is_pair() and self.is_3_of_a_kind()
 
-	def is_full_house(self):
-		pass
+    def is_4_of_a_kind(self):
+        ranks = list(map(lambda card: card.rank, self.cards))
+        counts = dict((rank, ranks.count(rank)) for rank in ranks)
+        return any(count == 4 for value, count in counts.items())
 
-	def is_4_of_a_kind(self):
-		pass
+    def is_straight_flush(self):
+        return self.is_flush() and self.is_straight()
 
-	def is_straight_flush(self):
-		pass
-
-	def is_royal_flush(self):
-		pass
+    def is_royal_flush(self):
+        return self.cards[0].value == 10 and self.is_straight() and self.is_flush()
 
 
 class Player:
 
-	def __init__(self,name):
-		self.chips = 100
-		self.name = name
+    def __init__(self, name):
+        self.chips = 100
+        self.name = name
 
-	def bet(self,amount):
-		if amount > self.chips:
-			 print("Sorry, your bet can't exceed",chips.total)
-			 return False
-		else:
-			self.chips -= amount
-			print("{} has {} chips remaining".format(self.name,self.chips))
-			return True
+    def bet(self, amount):
+        if amount > self.chips:
+            print("Sorry, your bet can't exceed", self.chips)
+            return False
+        else:
+            self.chips -= amount
+            print("{} has {} chips remaining".format(self.name, self.chips))
+            return True
+
 
 class Table:
 
-	def __init__(self):
-		self.pot = 0
+    def __init__(self, player):
+        self.pot = 0
+        self.player_hand = []
+        self.dealer_hand = []
+        self.board_cards = []
+        self.deck = Deck()
+        self.player = player
 
-	def new_game(self,player):
-		self.pot = 0
-		self.deck = Deck()
-		self.deck.shuffle()
+    def new_game(self):
+        self.deck = Deck()
+        self.player_hand.clear()
+        self.dealer_hand.clear()
+        self.board_cards.clear()
+        self.pot = 0
+        self.deck.shuffle()
 
-		self.player = player
-		
-		# if self.player.bet(10):
-			# self.pot += 10 * 2
+        # if self.player.bet(10):
+        # self.pot += 10 * 2
 
-		self.deal_hands()
+        self.deal_hands()
 
-	def deal_hands(self):
-		self.player_hand = []
-		self.dealer_hand = []
-		self.board_cards = []
+    def deal_hands(self):
+        self.player_hand.append(self.deck.deal_card())
+        self.dealer_hand.append(self.deck.deal_card())
 
-		self.player_hand.append(self.deck.deal_card())
-		self.dealer_hand.append(self.deck.deal_card())
+        self.player_hand.append(self.deck.deal_card())
+        self.dealer_hand.append(self.deck.deal_card())
 
-		self.player_hand.append(self.deck.deal_card())
-		self.dealer_hand.append(self.deck.deal_card())
-
-		self.board_cards.append(self.deck.deal_card())
-		self.board_cards.append(self.deck.deal_card())
-		self.board_cards.append(self.deck.deal_card())
-		self.board_cards.append(self.deck.deal_card())
-		self.board_cards.append(self.deck.deal_card())
+        self.board_cards.append(self.deck.deal_card())
+        self.board_cards.append(self.deck.deal_card())
+        self.board_cards.append(self.deck.deal_card())
+        self.board_cards.append(self.deck.deal_card())
+        self.board_cards.append(self.deck.deal_card())
 
 
 iteration = 0
 playing = True
 
-table = Table()
-# table.new_game(Player("Martynas"))
+p1 = Player("Martynas")
+table = Table(p1)
 
 while playing:
-	table.new_game(Player("Martynas"))
-	a = table.player_hand + table.board_cards
+    table.new_game()
+    a = table.player_hand + table.board_cards
 
-	hands = [Hand(cards) for cards in itertools.combinations(a, 5)]
+    hands = [Hand(cards) for cards in itertools.combinations(a, 5)]
 
-	for hand in hands:
-		if hand.is_flush():
-			print("{} after {} iterations".format(hand, iteration))
-			playing = False
+    for hand in hands:
+        if hand.is_full_house():
+            print("{} after {} iterations".format(hand, iteration))
+            playing = False
 
-	iteration += 1
-
+    iteration += 1
 
 # print("Current pot is ", table.pot)
 # print("--------------")
@@ -152,4 +169,4 @@ while playing:
 # hands = [Hand(cards) for cards in itertools.combinations(a, 5)]
 
 # for hand in hands:
-	# print(hand.combination())
+# print(hand.combination())
